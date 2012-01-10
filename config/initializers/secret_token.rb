66ccdb3a311dc421231b902591827dc3f911ccf9
Ivan Povalyukhin <ipoval@ya.ru>
2012-01-09 23:21:06 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learnix::Application.config.secret_token = '510db29b60fcc3ad878056e57ee3a9231f833b4819e59879333a2cf79f2c078308f7d83dc49b6d44771b794270f72be9ce54eb34b6ce1e7645ebc7fd7e7f5bde'
